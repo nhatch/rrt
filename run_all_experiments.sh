33221bc1b61bd7fef66342592b63b30245e256a1
Nathan Hatch <nathanhatch297@gmail.com>
2020-10-29 16:34:01 -0700

    Make it possible to run analysis separately

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -16,15 +16,4 @@ for DYNAMIC_OBSTACLES in 0 1; do
   done
 done
 
-echo
-echo "POINT ROBOT FIRST ORDER"
-awk -f count.awk final_results_pf.txt
-echo
-echo "POINT ROBOT SECOND ORDER"
-awk -f count.awk final_results_ps.txt
-echo
-echo "STICK ROBOT FIRST ORDER"
-awk -f count.awk final_results_sf.txt
-echo
-echo "STICK ROBOT SECOND ORDER"
-awk -f count.awk final_results_ss.txt
+./analyze.sh
